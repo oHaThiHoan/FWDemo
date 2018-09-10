cfd3bd1d0475323fc21beb5209a8b7bbe552074f
oHaThiHoan <ha.thi.hoan@framgia.com>
2018-09-10 11:00:52 +0700

    Add framework

---
+++ FWDemo/FWDemo.h
@@ -0,0 +1,19 @@
+//
+//  FWDemo.h
+//  FWDemo
+//
+//  Created by Ha Thi Hoan on 9/6/18.
+//  Copyright © 2018 Ha Thi Hoan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FWDemo.
+FOUNDATION_EXPORT double FWDemoVersionNumber;
+
+//! Project version string for FWDemo.
+FOUNDATION_EXPORT const unsigned char FWDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FWDemo/PublicHeader.h>
+
+
---
+++ FWDemo/MyLabel.swift
@@ -0,0 +1,15 @@
+//
+//  MyLabel.swift
+//  FWDemo
+//
+//  Created by Ha Thi Hoan on 9/6/18.
+//  Copyright © 2018 Ha Thi Hoan. All rights reserved.
+//
+
+import UIKit
+
+public class MyLabel: UILabel {
+    public func setText() {
+    self.text = "Hello"
+    }
+}
